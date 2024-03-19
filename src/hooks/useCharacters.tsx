47c6e936c1a64e381f85a4ca2de8fefd0b97a20d
Galarza Matías <matiasgg1821@gmail.com>
2024-03-19 19:52:26 -0300

    enviroments variables

--- src/hooks/useCharacters.tsx
+++ src/hooks/useCharacters.tsx
@@ -1,16 +1,19 @@
 import md5 from "md5";
 import {useEffect, useState} from "react";
 
-const publicKey = "6f3a767ba9d4f6da258039aab200e2df";
-const privateKey = "59074d2fd4ec963b60aeba994ace758b1038d7e1";
-
 export default function useCharacters() {
   const [characters, setCharacters] = useState();
   const timestamp = new Date().getTime().toString();
 
-  const hash = md5(timestamp + privateKey + publicKey);
+  const hash = md5(
+    timestamp +
+      import.meta.env.VITE_PRIVATE_KEY +
+      import.meta.env.VITE_PUBLIC_KEY
+  );
 
-  const apiUrl = `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicKey}&hash=${hash}`;
+  const apiUrl = `http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${
+    import.meta.env.VITE_PUBLIC_KEY
+  }&hash=${hash}`;
 
   useEffect(() => {
     fetch(apiUrl)
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    origin: "http://192.168.0.3:5173",
-  },
   assetsInclude: "**/*.md",
   plugins: [react()],
 });
